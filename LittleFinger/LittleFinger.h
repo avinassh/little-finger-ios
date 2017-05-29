878c3fde04cce7f47cb4159f1b8ab8841d5fed79
avi <hi@avi.im>
2017-05-29 20:07:56 +0530

    Inital import

---
+++ LittleFinger/LittleFinger.h
@@ -0,0 +1,19 @@
+//
+//  LittleFinger.h
+//  LittleFinger
+//
+//  Created by avi on 29/05/17.
+//  Copyright © 2017 avi.im. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LittleFinger.
+FOUNDATION_EXPORT double LittleFingerVersionNumber;
+
+//! Project version string for LittleFinger.
+FOUNDATION_EXPORT const unsigned char LittleFingerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LittleFinger/PublicHeader.h>
+
+
